d525401f0265f42d1e4cd5dfb390eaae86c9a821
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-01 18:03:12 +0800

    >  compile NEMU 2020244008 bronya Linux localhost.localdomain 5.10.134-13.an8.x86_64 #1 SMP Mon Jan 9 10:39:46 CST 2023 x86_64 x86_64 x86_64 GNU/Linux  18:03:12 up 1 day, 17:29,  1 user,  load average: 0.42, 0.15, 0.10 af195a40c5bab1fb4701c5866ac797fa88116079

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -19,8 +19,11 @@ static void init_log() {
 }
 
 static void welcome() {
-	printf("Welcome to NEMU!\n(You're right, but Honkai: Star Rail is a new turn-based game developed by miHoYo. The game takes place in a universe created by the [Star God], where they create reality to grind the stars and leave their mark on countless [worlds], where you will play as a special traveler who will follow the journey of a certain [Star God] with his companions who have inherited the consciousness of [Star God], ride the starry sky train through the galaxy, and embark on new adventures between countless strange [worlds] to explore new civilizations while gradually uncovering the truth of [Star God's Secrets].)\nThe executable is %s.\nFor help, type \"help\"\n",
-			exec_file);
+	 printf("\033[32mWelcome to NEMU!\033[0m\n"  
+           "(You're right, but Honkai: Star Rail is a new turn-based game developed by miHoYo. The game takes place in a universe created by the [Star God], where they create reality to grind the stars and leave their mark on countless [worlds], where you will play as a special traveler who will follow the journey of a certain [Star God] with his companions who have inherited the consciousness of [Star God], ride the starry sky train through the galaxy, and embark on new adventures between countless strange [worlds] to explore new civilizations while gradually uncovering the truth of [Star God's Secrets].)\n"  
+           "\033[31mThe executable is %s.\033[0m\n"  
+           "For help, type \"help\"\n",  
+           exec_file);  
 }
 
 void init_monitor(int argc, char *argv[]) {
