1aec8f74b1c0c2deb99536d5732e407b065c948b
tracer-NEMU2020 <tjuics2020@163.com>
2024-09-01 17:54:45 +0800

    >  compile NEMU 2020244008 bronya Linux localhost.localdomain 5.10.134-13.an8.x86_64 #1 SMP Mon Jan 9 10:39:46 CST 2023 x86_64 x86_64 x86_64 GNU/Linux  17:54:45 up 1 day, 17:20,  1 user,  load average: 0.08, 0.09, 0.09 c8803e38dffda249378b1db849e7749546a73c7a

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -19,7 +19,7 @@ static void init_log() {
 }
 
 static void welcome() {
-	printf("Welcome to NEMU!(你说的对，但是《崩坏：星穹铁道》是由米哈游自主研发的一款全新回合制游戏。游戏发生在一个「星神」所创造的宇宙，在这里，他们创造现实磨削星辰，在无数【世界】留下他们痕迹，在这里，你将扮演一位名为特殊的旅客，将与继承【开拓】意识的伙伴们，和他们一起沿着某个【星神】曾经所行之旅前进，乘坐星空列车穿越银河，在无数光怪陆离【世界】与【世界】之间展开新的冒险探索新的文明同时逐步发掘「星神秘密」的真相。)\nThe executable is %s.\nFor help, type \"help\"\n",
+	printf("Welcome to NEMU!\n(You're right, but Honkai: Star Rail is a new turn-based game developed by miHoYo. The game takes place in a universe created by the [Star God], where they create reality to grind the stars and leave their mark on countless [worlds], where you will play as a special traveler who will follow the journey of a certain [Star God] with his companions who have inherited the consciousness of [Star God], ride the starry sky train through the galaxy, and embark on new adventures between countless strange [worlds] to explore new civilizations while gradually uncovering the truth of [Star God's Secrets].)\nThe executable is %s.\nFor help, type \"help\"\n",
 			exec_file);
 }
 
