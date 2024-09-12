615ecbeac2aa8ede688c293ec0f6e123b05cfdd9
tracer-ics2016 <tracer@njuics.org>
2024-09-12 08:55:38 -0700

    >  compile NEMU 3023244344 k423 Linux ubuntu 5.4.0-84-generic #94~18.04.1-Ubuntu SMP Thu Aug 26 23:17:46 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  08:55:38 up  3:52,  1 user,  load average: 0.29, 0.07, 0.02 8de3ce4396150a99af1ed216428b19666cf0e73

--- nemu/include/cpu/helper.h
+++ nemu/include/cpu/helper.h
@@ -11,6 +11,9 @@
 static inline uint32_t instr_fetch(swaddr_t addr, size_t len) {
 	return swaddr_read(addr, len);//读内存
 }
+// 在 NEMU 中, exec()函数首先通过 instr_fetch()取出指令的第一
+// 个字节, 然后根据取到的这个字节查看opcode_table, 得到指令的helper函数, 
+// 从而调用这个 helper 函数来继续模拟这条指令的执行。
 
 /* Instruction Decode and EXecute */
 static inline int idex(swaddr_t eip, int (*decode)(swaddr_t), void (*execute) (void)) {  // 后两个函数指针表示译码和执行
